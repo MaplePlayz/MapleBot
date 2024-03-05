d66db40c9f6a09a92500f0c01dc4703736072e32
MaplePlayz <96486237+MaplePlayz@users.noreply.github.com>
2024-03-05 14:35:59 +0100

    ban commando toegvoegd

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ client = discord.Client(intents=intens)
 tree = app_commands.CommandTree(client)
 
 guild_id = 1173725609382400101  # Hier kun je de gewenste guild-ID plaatsen
+admin_roles = [1173725609382400106, 1173729989670223882]  # Hier kun je de gewenste admin-role ID's plaatsen
 
 @tree.command(
     name="hello",
@@ -21,8 +22,9 @@ async def first_command(interaction):
     description="Kick a user",
     guild=discord.Object(id=guild_id)
 )
+#kick commando
 async def kick(interaction, user: discord.Member, reason: str):
-    required_roles = [1173725609382400106, 1173729989670223882]
+    required_roles = admin_roles
     member_roles = [role.id for role in interaction.user.roles]
 
     if any(role_id in member_roles for role_id in required_roles):
@@ -31,8 +33,46 @@ async def kick(interaction, user: discord.Member, reason: str):
         await interaction.response.send_message(f"{user} is gekickt voor {reason}")
     else:
         await interaction.response.send_message("Je hebt hier geen premissies voor.")
+#ban commando
+@tree.command(
+    name="ban",
+    description="Ban a user",
+    guild=discord.Object(id=guild_id)
+)
+async def ban(interaction, user: discord.Member, reason: str):
+    required_roles = admin_roles
+    member_roles = [role.id for role in interaction.user.roles]
 
+    if any(role_id in member_roles for role_id in required_roles):
+        await user.send(f"Je bent geband van {interaction.guild.name} voor {reason}")
+        await user.ban(reason=reason)
+        await interaction.response.send_message(f"{user} is geband voor {reason}")
+    else:
+        await interaction.response.send_message("Je hebt hier geen premissies voor.")
 
+#unban commando
+# @tree.command(
+#     name="unban",
+#     description="Unban a user",
+#     guild=discord.Object(id=guild_id)
+# )
+# async def unban(interaction, user_id: int):
+#             required_roles = admin_roles
+#             member_roles = [role.id for role in interaction.user.roles]
+
+#             if any(role_id in member_roles for role_id in required_roles):
+#                 banned_users = await interaction.guild.bans()
+#                 for ban_entry in banned_users:
+#                     if ban_entry.user.id == int(user_id):  # Convert user_id to an integer
+#                         await interaction.guild.unban(ban_entry.user)
+#                         await interaction.response.send_message(f"Gebruiker met ID {user_id} kan de server weer in.")
+#                         return
+#                 await interaction.response.send_message(f"Gebruiker met ID {user_id} is niet verbannen.")
+#             else:
+#                 await interaction.response.send_message("Je hebt hier geen premissies voor.")
+
+    
+        
 
 
 
