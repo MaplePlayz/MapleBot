6fbe97bbc0c7529c54a276c92b790e64a9ec848f
MaplePlayz <96486237+MaplePlayz@users.noreply.github.com>
2024-03-05 13:13:10 +0100

    first command

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ client = discord.Client(intents=intens)
 tree = app_commands.CommandTree(client)
 
 @tree.command(
-    name="commandname",
+    name="hello",
     description="My first application Command",
-    guild=discord.Object(id=1173978170723151973)
+    guild=discord.Object(id=1173725609382400101)
 )
 async def first_command(interaction):
     await interaction.response.send_message("Hello!")
 
 @client.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=1173978170723151973))
+    await tree.sync(guild=discord.Object(id=1173725609382400101))
     print("Ready!")
 
 client.run("token")
