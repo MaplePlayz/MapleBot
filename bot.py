b40974a3c99f3f598f571939a0764ca585736bd7
MaplePlayz <96486237+MaplePlayz@users.noreply.github.com>
2024-03-11 16:31:30 +0100

    responds toegevoegd aan command, mute, unmute en clear command toegevoegd, tijd in banlist 1 uur opgeschoven zodat hij de actuelle tijd laat zien

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
-from datetime import datetime
 from discord import Option
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
+from datetime import datetime, timedelta
 
 admin_roles = [1173725609382400106, 1173729989670223882]  # Hier kun je de gewenste admin-role ID's plaatsen
 muted_role = 1197094790379089971  # Hier kun je de gewenste muted-role ID plaatsen
@@ -20,7 +20,7 @@ servers = [1173725609382400101]
 @commands.has_permissions(ban_members=True)
 async def banlist(ctx):
     await ctx.defer()
-    embed = discord.Embed(title="Banned Users", description="List of banned users", color=0x00ff00, timestamp=datetime.utcnow())
+    embed = discord.Embed(title="Banned Users", description="List of banned users", color=0x00ff00, timestamp=datetime.utcnow() + timedelta(hours=1))
     async for entry in ctx.guild.bans():
         if len(embed.fields) >= 25:
             break
@@ -52,7 +52,7 @@ async def unban(ctx, user: str = discord.Option(name="user", description="The us
 @bot.event
 async def on_application_command_error(ctx, error):
     if isinstance(error, MissingPermissions):
-        await ctx.send("You don't have the required permissions to run this command.")
+        await ctx.respond("You don't have the required permissions to run this command.")
     else:
         await ctx.respond(f"Something went wrong, I couldn't unban this member or the member isn't banned. Error: {error}")
         raise error
@@ -63,27 +63,27 @@ async def on_application_command_error(ctx, error):
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, member: Option(discord.Member, description="The member to ban"), reason: Option(str, description="why?", required=False)):
     if member.id == ctx.author.id:
-        await ctx.send("You can't ban yourself!")
+        await ctx.respond("You can't ban yourself!")
         return
     elif ctx.guild.get_member(member.id) and ctx.guild.get_member(member.id).guild_permissions.administrator:
-        await ctx.send("You can't ban an admin!")
+        await ctx.respond("You can't ban an admin!")
         return
     else:
         if reason is None:
             reason = f"No reason provided by {ctx.author.name}"
         await member.send(f"You have been banned from {ctx.guild.name} for {reason}")
         await ctx.guild.ban(member, reason=reason)
-        await ctx.send(f"{member.mention} has been banned by {ctx.author.mention} for {reason}")
+        await ctx.respond(f"You have banned {member.mention} for {reason}")
         log_channel = bot.get_channel(log_channel_id)
         await log_channel.send(f"{member.mention} has been banned by {ctx.author.mention} for {reason}")
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, MissingPermissions):
-        await ctx.send("You don't have the required permissions to run this command.")
+        await ctx.respond("You don't have the required permissions to run this command.")
     else:
-        await ctx.send(f"Something went wrong, I couldn't ban this member. Error: {error}")
+        await ctx.respond(f"Something went wrong, I couldn't ban this member. Error: {error}")
         raise error
-
+        
     
 #kick command
 
@@ -91,30 +91,89 @@ async def ban_error(ctx, error):
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, member: Option(discord.Member, description="The member to ban"), reason: Option(str, description="why?", required=False)):
     if member.id == ctx.author.id:
-        await ctx.send("You can't kick yourself!")
+        await ctx.respond("You can't kick yourself!")
         return
     elif ctx.guild.get_member(member.id) and ctx.guild.get_member(member.id).guild_permissions.administrator:
-        await ctx.send("You can't kick an admin!")
+        await ctx.respond("You can't kick an admin!")
         return
     else:
         if reason is None:
             reason = f"No reason provided by {ctx.author.name}"
         await member.send(f"You have been kicked from {ctx.guild.name} for {reason}")
         await ctx.guild.ban(member, reason=reason)
-        await ctx.send(f"{member.mention} has been kicked by {ctx.author.mention} for {reason}")
+        await ctx.respond(f"You have kicked {member.mention} for {reason}")
         log_channel = bot.get_channel(log_channel_id)
         await log_channel.send(f"{member.mention} has been kicked by {ctx.author.mention} for {reason}")
 @ban.error
 async def ban_error(ctx, error):
     if isinstance(error, MissingPermissions):
-        await ctx.send("You don't have the required permissions to run this command.")
+        await ctx.respond("You don't have the required permissions to run this command.")
     else:
-        await ctx.send(f"Something went wrong, I couldn't ban this member. Error: {error}")
+        await ctx.respond(f"Something went wrong, I couldn't ban this member. Error: {error}")
         raise error
 
+#mute command
+@bot.slash_command(guild_ids=servers, name="mute", description="Mute a user")
+@commands.has_permissions(manage_roles=True)
+async def mute(ctx, member: Option(discord.Member, description="The member to mute"), reason: Option(str, description="why?", required=False)):
+    if member.id == ctx.author.id:
+        await ctx.respond("You can't mute yourself!")
+        return
+    elif ctx.guild.get_member(member.id) and ctx.guild.get_member(member.id).guild_permissions.administrator:
+        await ctx.respond("You can't mute an admin!")
+        return
+    if discord.utils.get(member.roles, id=muted_role) is not None:
+        await ctx.respond(f"{member.mention} is already muted.")
+        return
+    else:
+        if reason is None:
+            reason = f"No reason provided by {ctx.author.name}"
+        await member.send(f"You have been muted in {ctx.guild.name} for {reason}")
+        await member.add_roles(ctx.guild.get_role(muted_role))
+        await ctx.respond(f"you have muted {member.mention} for {reason}")
+        log_channel = bot.get_channel(log_channel_id)
+        await log_channel.send(f"{member.mention} has been muted by {ctx.author.mention} for {reason}")
+@mute.error
+async def mute_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.respond("You don't have the required permissions to run this command.")
+    else:
+        await ctx.respond(f"Something went wrong, I couldn't mute this member. Error: {error}")
+        raise error
 
+#unmute command
+@bot.slash_command(guild_ids=servers, name="unmute", description="Unmute a user")
+@commands.has_permissions(manage_roles=True)
+async def unmute(ctx, member: Option(discord.Member, description="The member to unmute")):
+    if discord.utils.get(member.roles, id=muted_role) is not None:
+        
+        await member.remove_roles(ctx.guild.get_role(muted_role))
+        await ctx.respond(f"You have unmuted {member.mention}")
+        log_channel = bot.get_channel(log_channel_id)
+        await log_channel.send(f"{member.mention} has been unmuted by {ctx.author.mention}")
+    else:
+        await ctx.respond(f"{member.mention} is not muted.")
+@unmute.error
+async def unmute_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.respond("You don't have the required permissions to run this command.")
+    else:
+        await ctx.respond(f"Something went wrong, I couldn't unmute this member. Error: {error}")
+        raise error
 
-
+#clear command
+@bot.slash_command(guild_ids=servers, name="clear", description="Clear messages")
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount: Option(int, description="The amount of messages to clear")):
+    await ctx.channel.purge(limit=amount)
+    await ctx.respond(f"{amount} messages have been cleared")
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.respond("You don't have the required permissions to run this command.")
+    else:
+        await ctx.respond(f"Something went wrong, I couldn't clear messages. Error: {error}")
+        raise error
 
 
 
